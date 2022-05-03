9ca209e878a07e378eac68730ddf8c9886634bba
Arex360 <49303647+Arex360@users.noreply.github.com>
2022-05-02 21:35:44 -0700

    first

---
+++ lib/classes/StaticData.dart
@@ -0,0 +1,42 @@
+class StaticData {
+  static String harmony = "https://api.s0.b.hmny.io/";
+  static String polygon = "https://rpc-mumbai.matic.today";
+  static bool isMatic = true;
+  static String contract = "0x42709E040f2890A65d76d21D783179201899D1B0";
+  static String privateKey =
+      "adad5f7c0ab8bdc513d3ab30929238f99e8d88b35e5436a7ec121ceb891f50ee";
+  static var Specilizaion = [
+    'Skin Specialist',
+    'Urologist',
+    'ENT specialist',
+    'Eye Specialist',
+    'Dentist',
+    'Heart Specialist',
+    'General Physician',
+    'General Surgeon',
+    'Kidney Specialist',
+    'Child Specialist',
+    'Neurologist',
+    'Psychiatrist',
+    'Consultant Physician',
+    'Pulmonologist',
+    'Diabetes Specialist',
+  ];
+  static var SpecilizaionKey = [
+    'skin',
+    'urologist',
+    'ent',
+    'eye',
+    'dentist',
+    'heart',
+    'physician',
+    'surgeon',
+    'kidney',
+    'child',
+    'neurologist',
+    'psychiatrist',
+    'cphysician',
+    'pulmonologist',
+    'diabetes',
+  ];
+}
